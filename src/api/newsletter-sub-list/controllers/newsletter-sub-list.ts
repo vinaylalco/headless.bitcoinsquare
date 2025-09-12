fe6a81aecf4d146ccff99d594b9b5e0d99547b84
Vinay Lal <onethruten@gmail.com>
2025-09-12 22:25:28 +0800

    re-adding newsletter collection

---
+++ src/api/newsletter-sub-list/controllers/newsletter-sub-list.ts
@@ -0,0 +1,7 @@
+/**
+ * newsletter-sub-list controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::newsletter-sub-list.newsletter-sub-list');
---
+++ src/api/newsletter-sub-list/routes/newsletter-sub-list.ts
@@ -0,0 +1,7 @@
+/**
+ * newsletter-sub-list router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::newsletter-sub-list.newsletter-sub-list');
